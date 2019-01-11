cc859a26c2b21ef6cb3f5af28822c93264a84e09
shiyao19951113 <820591937@qq.com>
2019-01-11 16:36:01 +0800

    更新

--- src/demo/day02/_7_If.java
+++ src/demo/day02/_7_If.java
@@ -3,7 +3,7 @@ package demo.day02;
 public class _7_If {
     public static void main(String[] args) {
         int score = 90;
-
+//      注意if和else if的区别
         if (score > 85) {
             System.out.println("A");
         } else if (score > 70) {
--- src/demo/day02/_8_Switch.java
+++ src/demo/day02/_8_Switch.java
@@ -3,6 +3,9 @@ package demo.day02;
 public class _8_Switch {
     public static void main(String[] args) {
         char grade = 'A'; // char int String? JDK 1.7
+
+        // 注意加不加break的区别
+        
         switch (grade) {
             case 'A':
                 System.out.println("优");
