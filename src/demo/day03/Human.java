2ddb7fb8f4ac14f1e95141f7e40fc8eb898d0f53
shiyao19951113 <820591937@qq.com>
2019-01-12 09:28:15 +0800

    更新

---
+++ src/demo/day03/Human.java
@@ -0,0 +1,14 @@
+package demo.day03;
+
+public class Human {
+
+    // 1. 域：类似于变量的定义
+    String name;
+    int age;
+
+    // 2. 方法
+    // void：返回类型，没有返回任何值
+    void study(){
+
+    }
+}
---
+++ src/demo/day03/_1_Oop.java
@@ -0,0 +1,19 @@
+package demo.day03;
+
+/**
+ * OOP : Object Oriented Programming 面向对象的程序设计
+ */
+public class _1_Oop {
+
+// 类，域，方法，对象
+
+// 1. 类：抽象的概念
+// 共同的特征或属性
+// 共同的行为和功能
+// 人类：person people human human being mankind
+// name gender/sex age height weight...
+// eat sleep study work...
+
+// C++ Java Python
+
+}
